c2e8d7c59447d3c7a1b6b69ca2bac3c02d20d69f
Alexandre dupont <alexandredupont96@gmail.com>
2019-06-18 16:35:52 +0200

    Partie 1

--- 2/script.js
+++ 2/script.js
@@ -1,6 +1,18 @@
 $( document ).ready(function() {
 	alert("Je tiens juste a preciser que Jeanne est quelqu un de cool. Longue vie a twitch");
+	monTexte="Retiré du panier"
+	monTexte2="Ajouté au panier"
+
+	$(this).html(monTexte)
+
 	$("p").on( "click", function() {
-		$("p").addClass('red');	
+		if($( this ).hasClass('red')){
+			$( this ).removeClass('red')
+			$( this).html(monTexte);
+		}else{
+			$( this ).addClass('red')
+			$(this).html(monTexte2);
+		}	
+
 	});
 });
