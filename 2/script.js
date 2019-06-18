fa793a7e3d41a3de1bacad4dc8cdd0771a7bf1ed
Alexandre dupont <alexandredupont96@gmail.com>
2019-06-18 15:09:52 +0200

    Deuxième commit

--- 2/script.js
+++ 2/script.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() {
 	alert("Je tiens juste a preciser que Jeanne est quelqu un de cool. Longue vie a twitch");
-	$("p").addClass('red');
+	$("p").on( "click", function() {
+		$("p").addClass('red');	
+	});
 });
